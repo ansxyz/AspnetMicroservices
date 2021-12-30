ffe0a95d80d58500e16484d0db6c31a93f81f308
Anshuman Roy <01.anshuman@gmail.com>
2021-12-30 20:34:55 +0530

    Discount.API aproject created

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,15 +1,7 @@
-using Basket.API.Repositories;
-using System.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = "basketdb:6379";// Configuration.Getvalue<string>;
-});
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
